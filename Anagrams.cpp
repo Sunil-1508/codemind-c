36b4b0ba783f0afd800b26b4e2c2a7ff0a50891c
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-05-10 19:46:31 +0530

    Create Anagrams.cpp

---
+++ Anagrams.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    int c=0,s=0;
+    getline(cin,a);
+    getline(cin,b);
+    if(a.length()!=b.length())
+    {
+        c=1;
+    }
+    for(int i=0;i<a.length();i++)
+    {
+        for(int j=0;j<b.length();j++)
+        {
+            if(a[i]==b[i]) 
+            {
+              b[j]='1'; 
+              s++;
+            }
+        }
+    }
+    if(s==a.length()||c==0) cout<<"True";
+    else cout<<"False";
+}
