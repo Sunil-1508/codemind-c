82b7dddf46badac532be0927ea77d2f93ec24159
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2022-10-09 12:20:55 +0530

    #include<stdio.h> int main() {     int n,i,s=0,avr,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     scanf("%d",&a[i]);     for(i=0;i<n;i++)     s+=a[i];     avr=s/n;     for(i=0;i<n;i++)     {         if(avr>=a[i])         c++;     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0,avr,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    s+=a[i];
+    avr=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(avr>=a[i])
+        c++;
+    }
+    printf("%d",c);
+}
